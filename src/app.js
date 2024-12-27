4adb0cabf76821d7808e60d13055aa758dcda280
Nick Sneed <57041223+NickSneed@users.noreply.github.com>
2024-12-27 08:54:17 -0600

    removed view height code

--- src/app.js
+++ src/app.js
@@ -1,10 +1,8 @@
-import viewheight from "./modules/viewheight";
 import scrolling from "./modules/scrolling";
 import postgrid from "./modules/postgrid";
 import lightbox from "./modules/lightbox";
 
 // Initialize modules
-viewheight()
 lightbox();
 postgrid();
 scrolling();
