675b3d3b04c00f4d62a26e95f951578cff55629c
Nick Sneed <57041223+NickSneed@users.noreply.github.com>
2024-12-27 15:33:04 -0600

    updated downloader to include posts

--- build/downloadtumblr.ps1
+++ build/downloadtumblr.ps1
@@ -1,5 +1,39 @@
 $ProgressPreference='SilentlyContinue'
 
+function GetPostLinks($filePath) {
+    $content = Get-Content $filePath -Raw
+    $hrefSet = New-Object System.Collections.Generic.HashSet[string]
+
+    $pattern = 'https:\/\/bobrumbly\.com\/post\/(.*?)"'
+    $matches = [regex]::Matches($content, $pattern)
+    foreach ($match in $matches) {
+        $href = $match.Value -replace '"', ''
+        $hrefSet.Add($href) | Out-Null
+    }
+    return @($hrefSet)
+}
+
+function GetPostID($url) {
+    $pattern = '\d+.*?';
+    if ($url -match $pattern) {
+        return $($matches[0])
+    } else {
+        return ''
+    }
+}
+
+function ReplacePostLinks($content) {
+    $pattern = 'https:\/\/bobrumbly\.com\/post\/.*?"'
+    $matches = [regex]::Matches($content, $pattern)
+    foreach ($match in $matches) {
+        $oldlink = $match.Value -replace '"', ''
+        $newlink = GetPostID $match.Value
+        $newlink = $newlink + '.html'
+        $content = $content -replace $oldlink, $newlink
+    }
+    return $content
+}
+
 function ProcessHtmlContent($filePath, $url) {
     curl -o $filePath $url
 
@@ -22,17 +56,35 @@ function ProcessHtmlContent($filePath, $url) {
     # Re-add app scripts
     $content = $content -replace '<!-- script -->', '<script src="../dist/app.js"></script>'
 
-    # Replace links
-    $pattern = 'href="https:\/\/bobrumbly\.com\/post\/.*?"'
-    $content = $content -replace $pattern, 'href="post.html"'
+    # Replace home links
     $pattern = 'href="\/"'
     $content = $content -replace $pattern, 'href="home.html"'
-    
+
     Set-Content -Path $filePath -Value $content
+}
+
+# Define the path to the local folder
+$localFolderPath = "local"
+
+# Check if the folder exists
+if (Test-Path $localFolderPath) {
+    # Remove all files and subdirectories within the folder
+    Remove-Item "$localFolderPath\*" -Recurse -Force
+    Write-Host "Emptied the local folder: $localFolderPath"
+} else {
+    Write-Host "The folder does not exist: $localFolderPath"
 }
 
 # Process main page
 ProcessHtmlContent 'local/home.html' 'https://bobrumbly.com/'
 
-# Process post page
-ProcessHtmlContent 'local/post.html' 'https://bobrumbly.com/post/770964072259895296/mario'
+# Process post pages
+$linkArr = GetPostLinks 'local/home.html'
+foreach ($link in $linkArr) {
+    $postID = GetPostID($link);
+    $localFilePath = 'local/' + $postID + '.html'
+    ProcessHtmlContent $localFilePath $link
+}
+
+# Replace post links
+ReplacePostLinks 'local/home.html'
