79bba1c22185d77189af58de4b536015588db25a
Nick Sneed <57041223+NickSneed@users.noreply.github.com>
2024-12-27 11:19:19 -0600

    refactored tumblr downloader

--- build/downloadtumblr.ps1
+++ build/downloadtumblr.ps1
@@ -1,47 +1,32 @@
 $ProgressPreference='SilentlyContinue'
-curl -o tumblr/local.html https://bobrumbly.com/
 
-# Remap the cdn files to dist folder
-$content = Get-Content tumblr/local.html -Raw
-$content = $content -replace 'https://tumblrbobrumbly.pages.dev/', '../dist/'
+function ProcessHtmlContent($filePath, $url) {
+    curl -o $filePath $url
 
-# Remove all script tags
-$pattern = '<script\b[^>]*>((?s).*?)<\/script>'
-$content = $content -replace $pattern, ''
+    # Remap the cdn files to dist folder
+    $content = Get-Content $filePath -Raw
+    $content = $content -replace 'https://tumblrbobrumbly.pages.dev/', '../dist/'
 
-# Remove all noscript tags
-$pattern = '<noscript\b[^>]*>((?s).*?)<\/noscript>'
-$content = $content -replace $pattern, ''
+    # Remove all script tags
+    $pattern = '<script\b[^>]*>((?s).*?)<\/script>'
+    $content = $content -replace $pattern, ''
 
-## Remove all iframes
-$pattern = '<iframe\b[^>]*>((?s).*?)<\/iframe>'
-$content = $content -replace $pattern, ''
+    # Remove all noscript tags
+    $pattern = '<noscript\b[^>]*>((?s).*?)<\/noscript>'
+    $content = $content -replace $pattern, ''
 
-# Readd app scripts
-$content = $content -replace '<!-- script -->', '<script src="../dist/app.js"></script>'
+    ## Remove all iframes
+    $pattern = '<iframe\b[^>]*>((?s).*?)<\/iframe>'
+    $content = $content -replace $pattern, ''
 
-Set-Content -Path tumblr/local.html -Value $content
+    # Readd app scripts
+    $content = $content -replace '<!-- script -->', '<script src="../dist/app.js"></script>'
 
+    Set-Content -Path $filePath -Value $content
+}
 
-curl -o tumblr/local-post.html https://bobrumbly.com/post/770964072259895296/mario
+# Process main page
+ProcessHtmlContent 'tumblr/local.html' 'https://bobrumbly.com/'
 
-# Remap the cdn files to dist folder
-$content = Get-Content tumblr/local-post.html -Raw
-$content = $content -replace 'https://tumblrbobrumbly.pages.dev/', '../dist/'
-
-# Remove all script tags
-$pattern = '<script\b[^>]*>((?s).*?)<\/script>'
-$content = $content -replace $pattern, ''
-
-# Remove all noscript tags
-$pattern = '<noscript\b[^>]*>((?s).*?)<\/noscript>'
-$content = $content -replace $pattern, ''
-
-## Remove all iframes
-$pattern = '<iframe\b[^>]*>((?s).*?)<\/iframe>'
-$content = $content -replace $pattern, ''
-
-# Readd app scripts
-$content = $content -replace '<!-- script -->', '<script src="../dist/app.js"></script>'
-
-Set-Content -Path tumblr/local-post.html -Value $content
+# Process post page
+ProcessHtmlContent 'tumblr/local-post.html' 'https://bobrumbly.com/post/770964072259895296/mario'
