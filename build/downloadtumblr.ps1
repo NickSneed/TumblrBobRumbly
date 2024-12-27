47b916bb58a4cd4a12c137dbc8a306f5d00d43bb
Nick Sneed <57041223+NickSneed@users.noreply.github.com>
2024-12-27 11:11:42 -0600

    updated downloader to include post

--- build/downloadtumblr.ps1
+++ build/downloadtumblr.ps1
@@ -20,5 +20,28 @@ $content = $content -replace $pattern, ''
 # Readd app scripts
 $content = $content -replace '<!-- script -->', '<script src="../dist/app.js"></script>'
 
-
 Set-Content -Path tumblr/local.html -Value $content
+
+
+curl -o tumblr/local-post.html https://bobrumbly.com/post/770964072259895296/mario
+
+# Remap the cdn files to dist folder
+$content = Get-Content tumblr/local-post.html -Raw
+$content = $content -replace 'https://tumblrbobrumbly.pages.dev/', '../dist/'
+
+# Remove all script tags
+$pattern = '<script\b[^>]*>((?s).*?)<\/script>'
+$content = $content -replace $pattern, ''
+
+# Remove all noscript tags
+$pattern = '<noscript\b[^>]*>((?s).*?)<\/noscript>'
+$content = $content -replace $pattern, ''
+
+## Remove all iframes
+$pattern = '<iframe\b[^>]*>((?s).*?)<\/iframe>'
+$content = $content -replace $pattern, ''
+
+# Readd app scripts
+$content = $content -replace '<!-- script -->', '<script src="../dist/app.js"></script>'
+
+Set-Content -Path tumblr/local-post.html -Value $content
