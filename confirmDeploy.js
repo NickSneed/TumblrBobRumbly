e19421fde44c9c698162b6bb30388f8405737118
Nick Sneed <57041223+NickSneed@users.noreply.github.com>
2024-12-28 12:33:23 -0600

    added deployment confirmation

---
+++ confirmDeploy.js
@@ -0,0 +1,28 @@
+// confirmDeploy.js
+const readline = require('readline');
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+rl.question('Are you sure you want to deploy? (yes/no) ', (answer) => {
+    if (answer.toLowerCase() === 'yes') {
+        console.log('Deploying...');
+        const { exec } = require('child_process');
+        exec('npm run compile:sass && npm run compile:js && gh-pages -d dist --cname ghpages.bobrumbly.com', (err, stdout, stderr) => {
+            if (err) {
+                console.error(`Error: ${err.message}`);
+                return;
+            }
+            if (stderr) {
+                console.error(`stderr: ${stderr}`);
+                return;
+            }
+            console.log(`stdout: ${stdout}`);
+        });
+    } else {
+        console.log('Deployment canceled.');
+    }
+    rl.close();
+});
